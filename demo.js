4f8ff87a9b122d1242e7750adb09aefffb558fe4
David E. Flores <floresdavide0@gmail.com>
2021-04-20 20:48:09 -0600

    Se agrego la constante 'palabraGuiones'

--- demo.js
+++ demo.js
@@ -4,8 +4,11 @@ const palabras = ['hoja', 'casa', 'lago', 'caballo', 'corral'];
 //nos da una palabra aleatoria
 const palabra = palabras[Math.floor(Math.random() * palabras.length)]
 
+//cambia las palabras por guiones bajos
+const palabraGuiones = palabra.replace(/./g, "_ ");
 
 
+//funcion anonima para calcular palabra aleatoria
 document.querySelector('#evaluar').addEventListener('click', () => {
 
     
