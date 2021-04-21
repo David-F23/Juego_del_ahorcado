331b018944a3c04c6d044e5f8867d035e8855fb7
David E. Flores <floresdavide0@gmail.com>
2021-04-20 20:43:09 -0600

    creo una funcion anonima para calcular una palabra aleatoria, se creo la constante 'palabras' que contiene las palabras que estaran en el juego y se agrego la constante 'palabra' que nos dará una palabra aleatoria

---
+++ demo.js
@@ -0,0 +1,12 @@
+//Palabras que estaran en el juego
+const palabras = ['hoja', 'casa', 'lago', 'caballo', 'corral'];
+
+//nos da una palabra aleatoria
+const palabra = palabras[Math.floor(Math.random() * palabras.length)]
+
+
+
+document.querySelector('#evaluar').addEventListener('click', () => {
+
+    
+});
