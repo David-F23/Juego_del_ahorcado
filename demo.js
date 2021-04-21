61496eaf68846fd328a4d09d012b38d29e3a6077
David E. Flores <floresdavide0@gmail.com>
2021-04-20 21:22:40 -0600

    Se desarrollo la función anonima para que haga sus diferentes tareas

--- demo.js
+++ demo.js
@@ -1,6 +1,8 @@
+//función que reemplza la letra
 String.prototype.replaceAt=function(index, character) { 
 
     return this.substr(0, index) + character + this.substr(index+character.length); 
+    //uso: palabraGuiones = palabraGuiones.replaceAt(i*2, letra);
 } 
 
 
@@ -12,12 +14,24 @@ const palabras = ['hoja', 'casa', 'lago', 'caballo', 'corral'];
 const palabra = palabras[Math.floor(Math.random() * palabras.length)]
 
 //cambia las palabras por guiones bajos
-const palabraGuiones = palabra.replace(/./g, "_ ");
+let palabraGuiones = palabra.replace(/./g, "_ ");
 
+//se recibe el parrafo que pintara las letras
+document.querySelector('#salida').innerHTML = palabraGuiones;
 
 //funcion anonima para calcular palabra aleatoria
 document.querySelector('#evaluar').addEventListener('click', () => {
 
     const letra = document.querySelector('#letra').value;
-    alert(letra);
+
+    for(const i in palabra){
+
+        if(letra == palabra[i]){
+
+            palabraGuiones = palabraGuiones.replaceAt(i*2, letra);
+           
+        }
+    }
+
+    document.querySelector('#salida').innerHTML = palabraGuiones;
 });
